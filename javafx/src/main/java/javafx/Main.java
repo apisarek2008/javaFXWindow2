fd10c8b4fc523c32e07c444442665ceec9548263
Komputer <Komputer@DESKTOP-FBK86JR>
2024-05-18 20:03:58 +0200

    3

--- javafx/src/main/java/javafx/Main.java
+++ javafx/src/main/java/javafx/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("jnfgkrj");
 		System.out.println("hfgbjkewbfgekgbekgbejgbejgr");
+		System.out.println("jkrghekghreksdsd");
 
 	}
 
