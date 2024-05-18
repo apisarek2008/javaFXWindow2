9aad7d9c1fb0606b8887d50823b9f1a458085167
Komputer <Komputer@DESKTOP-FBK86JR>
2024-05-18 23:48:39 +0200

    button is null

---
+++ javafx/src/main/java/controller/StackPaneController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StackPaneController {
+
+	@FXML
+	private Button button;
+
+	public StackPaneController() {
+		System.out.println("gdkgkd");
+	}
+
+	@FXML
+	void initialize() {
+		button.setText("RRR");
+	}
+}
