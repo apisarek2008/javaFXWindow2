bac79db2509b9f8960b49dd6f32e6e9c7f8e5c0f
USER <apisarek2008@gmail.com>
2024-05-21 21:40:34 +0200

    21-05 21:40 - events

--- javafx/src/main/java/controller/StackPaneController.java
+++ javafx/src/main/java/controller/StackPaneController.java
@@ -1,26 +1,39 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.AnchorPane;
 
 public class StackPaneController {
 
-	@FXML
-	private Button button;
+    @FXML
+    private Button button;
 
-	public StackPaneController() {
-		
-	}
+    @FXML
+    private Button idRightButton;
+    
+    @FXML
+    private CheckBox chkBox;
 
-	@FXML
-	void initialize() {
-		button.setText("nowy Przycisk");
-	}
-	
-	@FXML
-	public void btnOnAction() {
-		System.out.println("button pressed");
-	}
-	
-	
+    @FXML
+    void btnOnAction(ActionEvent wypisz_btn_action) {
+    	System.out.println("Button on Action ");
+    }
+
+    @FXML
+    void btnText(ActionEvent wypisz_btnText) {
+    	System.out.println("btnText");
+    }
+    
+    @FXML
+    void klikChkBox(ActionEvent event) {
+    	System.out.println("Kli checkBox");
+    	if (chkBox.isPressed()) {
+    		System.out.println("CheckBox pressed");
+    	}
+    }
+
 }
+
