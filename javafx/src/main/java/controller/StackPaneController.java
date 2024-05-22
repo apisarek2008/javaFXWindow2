75ebb5d80a05d83cc688186e954d3f4a54965c55
Komputer <Komputer@DESKTOP-FBK86JR>
2024-05-22 12:11:03 +0200

    22.05 - 11:39 Event handler

--- javafx/src/main/java/controller/StackPaneController.java
+++ javafx/src/main/java/controller/StackPaneController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -8,32 +10,48 @@ import javafx.scene.layout.AnchorPane;
 
 public class StackPaneController {
 
-    @FXML
-    private Button button;
+	@FXML
+	private Button btn2;
+	
+	@FXML
+	private Button id_btnLeft;
+	@FXML
+	private CheckBox chkBox;
 
-    @FXML
-    private Button idRightButton;
-    
-    @FXML
-    private CheckBox chkBox;
+	@FXML
+	void initialize() {
 
-    @FXML
-    void btnOnAction(ActionEvent wypisz_btn_action) {
-    	System.out.println("Button on Action ");
-    }
+		EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
 
-    @FXML
-    void btnText(ActionEvent wypisz_btnText) {
-    	System.out.println("btnText");
-    }
-    
-    @FXML
-    void klikChkBox(ActionEvent event) {
-    	System.out.println("Kli checkBox");
-    	if (chkBox.isPressed()) {
-    		System.out.println("CheckBox pressed");
-    	}
-    }
+			@Override
+			public void handle(ActionEvent arg0) {
+				wypisz_wymaluj();
+
+			}
+
+		};
+		EventHandler<ActionEvent> act_btnLeft = new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				btn2_event();
+				
+			}
+		};
+
+		btn2.addEventHandler(ActionEvent.ACTION, handler);
+		id_btnLeft.addEventHandler(ActionEvent.ACTION, act_btnLeft);
+
+	}
+
+	protected void btn2_event() {
+		System.out.println("btn2 handler activated");
+
+	}
+
+	protected void wypisz_wymaluj() {
+		System.out.println("Wypisz wymaluj");
+
+	}
 
 }
-
